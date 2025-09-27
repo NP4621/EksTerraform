525a6988b7ddd80861a51cd0101a00d6f249b2be
NP4621 <nagaraj.puranik@zebra.com>
2025-09-27 16:16:48 +0530

    my first commit

---
+++ vpc.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+data "aws_availability_zones" "available" {}
+
+locals {
+  cluster_name = "abhi-eks-${random_string.suffix.result}"
+}
+
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
+}
+
